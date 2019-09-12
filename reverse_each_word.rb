9f1b4ec94cfd33c566f8145fc77861bea6a84f43
Urfan Guliyev <urfan196@yahoo.com>
2019-09-12 21:30:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word (sentence)
   string_array.each do |word|
     reverse_array.push(word.reverse)
   end
-  return reverse_array
+   reverse_array
 end
 
 def reverse_each_word (sentence)
@@ -14,5 +14,5 @@ def reverse_each_word (sentence)
   string_array.collect do |word|
     reverse_array.push(word.reverse)
   end
-  return reverse_array
+   reverse_array
 end
